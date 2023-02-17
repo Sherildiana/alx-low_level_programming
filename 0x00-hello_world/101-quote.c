362371aa109c2733ac06640acdb68cd92c6d2005
sherildiana <sherildiana@gmail.com>
2023-02-17 00:42:28 -0800

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdstring>
+/**
+ * main - c program that prints a line to the standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
