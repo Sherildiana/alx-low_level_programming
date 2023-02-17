c7ee1dbd3e8aae0d0157020bf8a435e57460ced9
sherildiana <sherildiana@gmail.com>
2023-02-17 15:10:47 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: always o (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'n'
+	while
+		(c <= 'z') {
+			putcher(c);
+			c++;
+		}
+	putcher('\n');
+	return (0);
+}
