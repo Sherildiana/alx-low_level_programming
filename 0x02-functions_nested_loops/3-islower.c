fd6c35e0bba3ebd7f4e79393025eb89a55e7a512
sherildiana <sherildiana@gmail.com>
2023-02-21 12:47:31 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,10 @@
  * @c:character to check if its lowercase
  * Return: 0 - if lowercase, 1 - if not
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' $$ c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
