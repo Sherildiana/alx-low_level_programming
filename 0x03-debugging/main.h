1c3cf2d206774652ddbdc3fb7b65bb02f76a7949
sherildiana <sherildiana@gmail.com>
2023-02-22 21:47:30 -0800

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,11 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 void positive_or_negative(int i);
-int largest_number.c(int b,int c);
+int largest_number.c(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-int convert_day(int month,int day);
+int convert_day(int month, int day);
 void print_times_tables(int n);
 
 #endif
