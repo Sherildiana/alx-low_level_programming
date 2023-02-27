9b5f8cab48bab7f6dd962dfced979e01e72fd202
sherildiana <sherildiana@gmail.com>
2023-02-27 12:10:31 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,27 +1,13 @@
 #include "main.h"
-
 /**
- * _puts - function declaration
- * @str: string pointer
- * Return: void
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-
-void _puts(char *str);
-
-/**
- * _puts - function definition
- * Description: prints a string
- * @str: the string pointer
- * Return: void
- */
-
 void _puts(char *str)
 {
-	int index;
-
-	for (index = 0; *(str + index) != '\0'; index++)
+	while (*str != '\0')
 	{
-		_putchar(*(str + index));
+		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
