f22adb84f7fd4705a10fac25e055bfc599ea6a4a
sherildiana <sherildiana@gmail.com>
2023-02-27 12:56:54 -0800

    7-pit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * Structs,enums and unions definations
- * Typedefs
- * Funtion prototype
- */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int_putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,4 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 #endif
