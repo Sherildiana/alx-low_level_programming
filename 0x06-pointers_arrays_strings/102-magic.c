dc0cab69dc0618038a365f634c810536ff5a0aee
sherildiana <sherildiana@gmail.com>
2023-03-02 12:15:28 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,49 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_buffer - prints buffers
- * @b: beffer
- * @size: buffer size
- *
- * Return: none
- */
-void print_buffer(char *b, int size)
+
+int main(void)
 {
-        int buf1, buf2, buf3;
+  int n;
+  int a[5];
+  int *p;
 
-        buf1 = 0;
-
-        if (size <= 0)
-        {
-                printf("\n");
-                return;
-        }
-        while (buf1 < size)
-        {
-                buf2 = size - buf1 < 10 ? size - buf1 : 10;
-                printf("%08x: ", buf1);
-                for (buf3 = 0; buf3 < 10; buf3++)
-                {
-                        if (buf3 < buf2)
-                                printf("%02x", *(b + buf1 + buf3));
-                        else
-                                printf("  ");
-                        if (buf3 % 2)
-                        {
-                                printf(" ");
-                        }
-                }
-                for (buf3 =  0; buf3 < buf2; buf3++)
-                {
-int integer = *(b + buf1 + buf3);
-
-                        if (integer < 32 || integer > 132)
-                        {
-                                integer = ',';
-                        }
-                        printf("%c", integer);
-                }
-                printf("\n");
-                buf1 += 10;
-        }
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
