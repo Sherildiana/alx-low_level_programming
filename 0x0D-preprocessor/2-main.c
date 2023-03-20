81599cc35c22d6e8780987a6fd6f8cdc42fda9b7
sherildiana <sherildiana@gmail.com>
2023-03-20 13:39:38 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-/**
-* main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
-int ain(void)
-{
-       printf("%s\n", __FILE__);
-       return (0);
-}
+Include stdio
+Int main void
+Printf("%s\n", _FILE_);
+Return (0);
